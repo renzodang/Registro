4c1c46be2b8308de0fec5ee248efe48f166311bd
Renzo D`Angelo <renzodang@gmail.com>
2022-08-21 21:41:40 -0300

    se

--- js/app.js
+++ js/app.js
@@ -1,28 +1,19 @@
 
-let lista =[];
-
 function desplegar(datos) {
-  
   let contenido = "";
   for (let dato of datos) {
     contenido +=
-    "<tr> <th>Ciudad</th> <th>Precio</th></tr>"+"<tr><td>" +
-      dato.ciudad +
-      "</td><td>" +
-      dato.precio +
-      "</td></tr>";
-  };
+     "<div> <label>" +
+      dato.ciudad + "</label><input type='number'>" +"<label>"+ dato.precio +
+      "</label></div>";
+  }
   document.getElementById("lista").innerHTML = contenido;
-};
+}
 
-
-
-
-document.addEventListener("DOMContentLoaded",()=>{
-  fetch ("/Carpeta/MOCK_DATA.json")
-  .then ((response) => response.json())
-  .then ((json) => {
-    desplegar(json)
-  });
-  
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("/Carpeta/MOCK_DATA.json")
+    .then((response) => response.json())
+    .then((json) => {
+      desplegar(json);
+    });
+});
